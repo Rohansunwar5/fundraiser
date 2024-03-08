bb045a8b97b6f16aedf9a92c372b6348c0d391f4
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-03-08 23:28:09 +0530

    bugs

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,8 +3,8 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
+  username: { type: String, unique: true },
+  firstName: { type: String },
   lastName: {type: String, required: true },
   photo: { type: String, required: true },
 })
